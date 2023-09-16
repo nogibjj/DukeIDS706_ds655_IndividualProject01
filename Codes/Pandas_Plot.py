03b29adadf083ca571c2d7839f88a82ab2e50d24
Divya Sharma <143483773+DivyaSharma0795@users.noreply.github.com>
2023-09-16 06:37:27 +0000

    Added Jupyter Notebook with python functions

--- Codes/Pandas_Plot.py
+++ Codes/Pandas_Plot.py
@@ -1,14 +1,25 @@
 #   Importing Packages
 import matplotlib.pyplot as plt
 
+
+#  Creating a function to build a scatterplot for the Iris dataset
 def PandasPlot(df):
-    groups = df.groupby('species')
-    fig,ax = plt.subplots()
+    groups = df.groupby("species")
+    fig, ax = plt.subplots()
     for name, group in groups:
-        ax.plot(group.sepal_length, group.sepal_width, marker='o', linestyle='', ms=12, label=name)
+        ax.plot(
+            group.sepal_length,
+            group.sepal_width,
+            marker="o",
+            linestyle="",
+            ms=12,
+            label=name,
+        )
     ax.legend()
-    fig.suptitle('Distribution of Sepal Length and Sepal Width across different species')
-    plt.xlabel('Sepal Length')
-    plt.ylabel('Sepal Width')
-    plt.savefig('./Resources/plot image.png')
+    fig.suptitle(
+        "Distribution of Sepal Length and Sepal Width across different species"
+    )
+    plt.xlabel("Sepal Length")
+    plt.ylabel("Sepal Width")
+    plt.savefig("./Resources/plot image.png")
     plt.show()
